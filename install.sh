8a5d5450ff298d7ec2397eee2d962913c8ab6aa7
仙丝暮成雪 <102725537+hujixiang0402@users.noreply.github.com>
2024-12-06 17:03:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt install -y python3 python3-pip python3-venv git wget
 # 克隆 GitHub 仓库
 echo "克隆 GitHub 仓库..."
 git clone https://github.com/hujixiang0402/NC_Reset.git
-cd reset_vserver
+cd NC_Reset
 
 # 创建虚拟环境并激活
 echo "创建虚拟环境..."
